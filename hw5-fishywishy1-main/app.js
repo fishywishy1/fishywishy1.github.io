1fa5be88a64406b9b4300341f08309c940022094
fishywishy1 <fdziadosz@wisc.edu>
2024-10-14 14:37:49 -0500

    Update app.js

--- hw5-fishywishy1-main/app.js
+++ hw5-fishywishy1-main/app.js
@@ -1,4 +1,4 @@
-console.log("Hello World!")
+console.log("Meow!")
 
 function duel() {
     const p1Health = getAttributeValue("p1-health");
@@ -18,9 +18,9 @@ function duel() {
     const p2TimeToDefeat = (p2Health + p2Armor + p2Armor) / p1DPS;
     const comp = p1TimeToDefeat - p2TimeToDefeat;
 
-    if (comp > 0){
+    if (p1TimeToDefeat < p2TimeToDefeat){
         alert("player 1 will win in " + Math.round(p2TimeToDefeat) + "seconds!");
-    } if (comp < 0){
+    } if (p1TimeToDefeat > p2TimeToDefeat){
         alert("player 2 will win in " + Math.round(p1TimeToDefeat) + "seconds!");
     } else {
         alert("Round ends in a draw");
